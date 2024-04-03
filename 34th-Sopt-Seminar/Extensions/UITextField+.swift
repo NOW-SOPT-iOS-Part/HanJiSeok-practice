c4e1b9566df105eecc0cac644d41379c29d860cd
sozohoy <sozohoy@gmail.com>
2024-04-03 10:51:57 +0900

    feat: LeftPadding 메소드(#1)

--- 34th-Sopt-Seminar/Extensions/UITextField+.swift
+++ 34th-Sopt-Seminar/Extensions/UITextField+.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+extension UITextField {
+    func leftPadding() {
+        let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 23, height: self.frame.height))
+        self.leftView = leftPaddingView
+        self.leftViewMode = ViewMode.always
+    }
+}
