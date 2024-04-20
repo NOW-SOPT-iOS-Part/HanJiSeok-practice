3487788f7f1ca09aa5db41e2cfcf142614c0181f
sozohoy <sozohoy@gmail.com>
2024-04-20 14:33:19 +0900

    add: View / Controller 분리

---
+++ 34th-Sopt-Seminar/View/LoginView.swift
@@ -0,0 +1,12 @@
+//
+//  LoginView.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 한지석 on 4/20/24.
+//
+
+import Cocoa
+
+class LoginView: UIView {
+
+}
---
+++ 34th-Sopt-Seminar/ViewController/LoginViewControllerWithMVC.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewControllerWithMVC.swift
+//  34th-Sopt-Seminar
+//
+//  Created by 한지석 on 4/20/24.
+//
+
+import Foundation
