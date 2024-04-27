86ee3e6b029096f9a4de0682151d3684d9e33419
sozohoy <sozohoy@gmail.com>
2024-04-27 15:28:34 +0900

    feat: 네트워크 기능 파일 추가(#8)

--- 34th-Sopt-Seminar/Week4/Network/APITarget+User.swift
+++ 34th-Sopt-Seminar/Week4/Network/APITarget+User.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+import Moya
+
+extension APITarget {
+    enum User {
+        case getUserInfo(memberId: String)
+        case signUp(request: SignUpRequestModel)
+    }
+}
+
+
+extension APITarget.User: TargetType {
+    var baseURL: URL {
+        return URL(string: Config.baseURL)!
+    }
+
+    var path: String {
+        switch self {
+        case .signUp:
+            return "/member/join"
+        case .getUserInfo(memberId: let memberId):
+            return "/member/info"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .signUp:
+            return .post
+        case .getUserInfo:
+            return .get
+        }
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .signUp(let request):
+            return .requestJSONEncodable(request)
+        case .getUserInfo:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String : String]? {
+        switch self {
+        case.signUp:
+            return ["Content-Type": "application/json"]
+        case .getUserInfo(let memberId):
+            return ["Content-Type": "application/json",
+                    "memberId" : memberId]
+        }
+
+    }
+}
+
+
