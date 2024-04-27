856641923d668c8f93ea03c2fdaf95c12174920a
sozohoy <sozohoy@gmail.com>
2024-04-27 16:54:09 +0900

    refactor: 에러 메세지 분리(#8)

--- 34th-Sopt-Seminar/Week4/ViewController/CheckUserInfoViewController.swift
+++ 34th-Sopt-Seminar/Week4/ViewController/CheckUserInfoViewController.swift
@@ -27,21 +27,12 @@ final class CheckUserInfoViewController: UIViewController {
         UserService.shared.getUserInfo(memberId: "\(11)") { [weak self] response in
             switch response {
             case .success(let data):
-                guard let data = data as? UserInfoResponseModel else {
-                    return }
+                guard let data = data as? UserInfoResponseModel else { return }
                 self?.idLabel.text = data.data.authenticationId
                 self?.nickNameLabel.text = data.data.nickname
                 self?.phoneNumberLabel.text = data.data.phone
-            case .requestErr:
-                print("요청 오류 입니다")
-            case .decodedErr:
-                print("디코딩 오류 입니다")
-            case .pathErr:
-                print("경로 오류 입니다")
-            case .serverErr:
-                print("서버 오류입니다")
-            case .networkFail:
-                print("네트워크 오류입니다")
+            default:
+                response.statusDescription()
             }
         }
     }
